dd78eee9cdafca11e385676508e9c52a73a2bd99
Bottesz <botyaring@gmail.com>
2024-01-16 10:08:21 +0100

    ossz tetel

---
+++ osszegzes.py
@@ -0,0 +1,12 @@
+def osszegzes_tetel():
+    n:int = 0
+    i:int = 0
+    ossz = 0
+    n:int = int(input("N: "))
+    while n < 0:
+        if i > n:
+            ossz += 1 
+    print(f"Az első db természetes szám összege: {ossz}")
+
+
+
